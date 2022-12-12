d03a07087f94fe180e6a62707d5f1001ef3bbb42
Leonid Borisevich <dazzbourgh@gmail.com>
2022-12-11 23:19:53 -0600

    task 10 part 2

--- src/main/kotlin/solution/Solution10.kt
+++ src/main/kotlin/solution/Solution10.kt
@@ -1,6 +1,7 @@
 package solution
 
 import arrow.core.Either
+import arrow.core.left
 import arrow.core.right
 
 object Solution10 : Solution {
@@ -14,12 +15,15 @@ object Solution10 : Solution {
         Noop -> sequenceOf(0)
     }
 
-    override fun solve1(input: Sequence<String>): Either<String, Number> =
+    private fun steps(input: Sequence<String>) =
         input.map { it.toCommand() }
             .flatMap { it.toSequence() }
             .let { sequenceOf(0) + it }
-            .take(220)
+            .take(240)
             .runningFold(1) { acc, increment -> acc + increment }
+
+    override fun solve1(input: Sequence<String>): Either<String, Number> =
+        steps(input)
             .mapIndexed { index, value -> index to (index * value) }
             .filter { (index, _) -> index == 20 || (index - 20) % 40 == 0 }
             .map { it.second }
@@ -27,7 +31,14 @@ object Solution10 : Solution {
             .sum()
             .right()
 
-    override fun solve2(input: Sequence<String>): Either<String, Number> {
-        return 1.right()
-    }
+    private fun List<Int>.toLine() =
+        mapIndexed { index, sprite -> if (index in (sprite - 1..sprite + 1)) ':' else ' ' }
+            .joinToString("")
+
+    override fun solve2(input: Sequence<String>): Either<String, Number> =
+        steps(input).drop(1)
+            .chunked(40)
+            .map { it.toLine() }
+            .joinToString("\n", prefix = "\n")
+            .left()
 }
